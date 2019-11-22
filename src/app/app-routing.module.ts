6823e365d07e65a915f701293c4f6d5bea53157d
Alejandro Mora <alejomg27@gmail.com>
2019-11-22 12:00:49 -0500

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ListComponent} from './list/list.component';
+import {ChangeComponent} from './change/change.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent
+  },
+  {
+    path: 'change',
+    component: ChangeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
